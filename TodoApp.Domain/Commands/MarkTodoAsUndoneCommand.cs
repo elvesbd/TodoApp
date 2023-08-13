a1cb1b5d7745e99a4e7edac8942914387201fd5e
elvesbd <elvesbd@yahoo.com>
2023-08-13 11:18:39 -0300

    feat: implemented validation params in mark to as undone command

---
+++ TodoApp.Domain/Commands/MarkTodoAsUndoneCommand.cs
@@ -0,0 +1,29 @@
+using Flunt.Notifications;
+using Flunt.Validations;
+using TodoApp.Domain.Commands.Interfaces;
+
+namespace TodoApp.Domain.Commands;
+
+public class MarkTodoAsUndoneCommand : Notifiable<Notification>, ICommand
+{
+    public MarkTodoAsUndoneCommand()
+    { }
+
+    public MarkTodoAsUndoneCommand(Guid id, string user)
+    {
+        Id = id;
+        User = user;
+    }
+
+    public Guid Id { get; set; }
+    public string User { get; set; }
+
+    public void Validate()
+    {
+        AddNotifications(
+            new Contract<MarkTodoAsUndoneCommand>()
+                .Requires()
+                .IsLowerOrEqualsThan(User, 6, "User", "Invalid user!")
+        );
+    }
+}
