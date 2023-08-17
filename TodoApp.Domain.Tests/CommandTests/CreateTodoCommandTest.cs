cec976121db5162aa4f527d7f603f724f69dbdb9
elvesbd <elvesbd@yahoo.com>
2023-08-17 17:15:37 -0300

    chore: add setup for unist tests

---
+++ TodoApp.Domain.Tests/CommandTests/CreateTodoCommandTest.cs
@@ -0,0 +1,17 @@
+namespace TodoApp.Domain.Tests.CommandsTests;
+
+[TestClass]
+public class CreateTodoCommandTests
+{
+    [TestMethod]
+    public void Given_an_invalid_command()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Given_an_valid_command()
+    {
+        Assert.Fail();
+    }
+}
