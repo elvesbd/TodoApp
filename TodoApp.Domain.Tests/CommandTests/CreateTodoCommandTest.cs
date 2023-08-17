831f96e0515e9f75c970af66577ee01a8fa47d9b
elvesbd <elvesbd@yahoo.com>
2023-08-17 17:25:56 -0300

    test: given an valid command

--- TodoApp.Domain.Tests/CommandTests/CreateTodoCommandTest.cs
+++ TodoApp.Domain.Tests/CommandTests/CreateTodoCommandTest.cs
@@ -12,4 +12,17 @@ public class CreateTodoCommandTests
         command.Validate();
         Assert.AreEqual(command.IsValid, false);
     }
+
+    [TestMethod]
+    public void Given_an_valid_command()
+    {
+        var command = new CreateTodoCommand
+        (
+           "Create Repo",
+           "Elves Brito",
+           DateTime.Now
+        );
+        command.Validate();
+        Assert.AreEqual(command.IsValid, true);
+    }
 }
