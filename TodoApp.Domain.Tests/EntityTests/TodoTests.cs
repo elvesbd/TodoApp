3d67b26d0c699a6aac09cb925de0bedb2081bfd0
elvesbd <elvesbd@yahoo.com>
2023-08-18 11:58:59 -0300

    test: given creating a todo marked as done done should be true

--- TodoApp.Domain.Tests/EntityTests/TodoTests.cs
+++ TodoApp.Domain.Tests/EntityTests/TodoTests.cs
@@ -1,3 +1,5 @@
+using TodoApp.Domain.Entities;
+
 namespace TodoApp.Domain.Tests.EntityTests;
 
 [TestClass]
@@ -6,6 +8,15 @@ public class TodoTests
     [TestMethod]
     public void Given_creating_a_todo_done_must_be_false()
     {
-        Assert.Fail();
+        var todo = new Todo("Create Repo", "Elves Brito", DateTime.Now);
+        Assert.IsFalse(todo.Done);
+    }
+
+    [TestMethod]
+    public void Given_creating_a_todo_marked_as_done_done_should_be_true()
+    {
+        var todo = new Todo("Create Repo", "Elves Brito", DateTime.Now);
+        todo.MarkAsDone();
+        Assert.IsTrue(todo.Done);
     }
 }
