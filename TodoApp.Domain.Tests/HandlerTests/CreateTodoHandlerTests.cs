46c1af288377e98fc85d14b08d7285f49accfce7
elvesbd <elvesbd@yahoo.com>
2023-08-18 11:20:49 -0300

    test: given an valid command must create an todo

--- TodoApp.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ TodoApp.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -1,3 +1,7 @@
+using TodoApp.Domain.Commands;
+using TodoApp.Domain.Handlers;
+using TodoApp.Domain.Tests.Repositories;
+
 namespace TodoApp.Domain.Tests.HandlerTests;
 
 [TestClass]
@@ -6,12 +10,18 @@ public class CreateTodoHandlerTests
     [TestMethod]
     public void Given_an_invalid_command_must_stop_execution()
     {
-        Assert.Fail();
+        var command = new CreateTodoCommand("", "", DateTime.Now);
+        var handler = new TodoHandler(new FakeTodoRepository());
+        var result = (GenericCommandResult)handler.Handle(command);
+        Assert.AreEqual(result.Success, false);
     }
 
     [TestMethod]
     public void Given_an_valid_command_must_create_an_todo()
     {
-        Assert.Fail();
+        var command = new CreateTodoCommand("Create Repo", "Elves Brito", DateTime.Now);
+        var handler = new TodoHandler(new FakeTodoRepository());
+        var result = (GenericCommandResult)handler.Handle(command);
+        Assert.AreEqual(result.Success, true);
     }
 }
