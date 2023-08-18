bc9fec2819faa00d2b5e9994fdbb64c90a221557
elvesbd <elvesbd@yahoo.com>
2023-08-18 14:52:31 -0300

    feat: implemented handle for mark todo as done

--- TodoApp.Domain/Handlers/TodoHandler.cs
+++ TodoApp.Domain/Handlers/TodoHandler.cs
@@ -7,7 +7,11 @@ using TodoApp.Domain.Repositories;
 
 namespace TodoApp.Domain.Handlers;
 
-public class TodoHandler : Notifiable<Notification>, IHandler<CreateTodoCommand>
+public class TodoHandler :
+    Notifiable<Notification>,
+    IHandler<CreateTodoCommand>,
+    IHandler<UpdateTodoCommand>,
+    IHandler<MarkTodoAsDoneCommand>
 {
     private readonly ITodoRepository _repository;
 
@@ -30,5 +34,39 @@ public class TodoHandler : Notifiable<Notification>, IHandler<CreateTodoCommand>
         var todo = new Todo(command.Title, command.User, command.Date);
         _repository.Create(todo);
         return new GenericCommandResult(true, "Tarefa salva com sucesso!", todo);
+    }
+
+    public ICommandResult Handle(UpdateTodoCommand command)
+    {
+        command.Validate();
+        if (!command.IsValid)
+            return new GenericCommandResult
+            (
+                false,
+                "Ops, parece que sua tarefa está errada!",
+                command.Notifications
+            );
+
+        var todo = _repository.GetById(command.Id, command.User);
+        todo.UpdateTitle(command.Title);
+        _repository.Update(todo);
+        return new GenericCommandResult(true, "Tarefa salva con sucesso!", todo);
+    }
+
+    public ICommandResult Handle(MarkTodoAsDoneCommand command)
+    {
+        command.Validate();
+        if (!command.IsValid)
+            return new GenericCommandResult
+            (
+                false,
+                "Ops, parece que sua tarefa está errada!",
+                command.Notifications
+            );
+
+        var todo = _repository.GetById(command.Id, command.User);
+        todo.MarkAsDone();
+        _repository.Update(todo);
+        return new GenericCommandResult(true, "Tarefa salva con sucesso!", todo);
     }
 }
