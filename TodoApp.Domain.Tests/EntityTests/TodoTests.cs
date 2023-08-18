e45f2f348f54e9bb245f50e74409fd5ce28db05d
elvesbd <elvesbd@yahoo.com>
2023-08-18 11:39:50 -0300

    test: created setup for unit tests

---
+++ TodoApp.Domain.Tests/EntityTests/TodoTests.cs
@@ -0,0 +1,11 @@
+namespace TodoApp.Domain.Tests.EntityTests;
+
+[TestClass]
+public class TodoTests
+{
+    [TestMethod]
+    public void Given_creating_a_todo_done_must_be_false()
+    {
+        Assert.Fail();
+    }
+}
