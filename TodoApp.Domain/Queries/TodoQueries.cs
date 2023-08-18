5c855098413c78cdbd6b1afd3f318cc5b139ccdf
elvesbd <elvesbd@yahoo.com>
2023-08-18 16:28:30 -0300

    feat: created query for get all

---
+++ TodoApp.Domain/Queries/TodoQueries.cs
@@ -0,0 +1,12 @@
+using System.Linq.Expressions;
+using TodoApp.Domain.Entities;
+
+namespace TodoApp.Domain.Queries;
+
+public static class TodoQueries
+{
+    public static Expression<Func<Todo, bool>> GetAll(string user)
+    {
+        return x => x.User == user;
+    }
+}
