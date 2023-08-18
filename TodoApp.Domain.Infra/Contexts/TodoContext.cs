9bde0829548583d584fba588a2f04b827b571d0a
elvesbd <elvesbd@yahoo.com>
2023-08-18 16:03:28 -0300

    feat: created mappings

--- TodoApp.Domain.Infra/Contexts/TodoContext.cs
+++ TodoApp.Domain.Infra/Contexts/TodoContext.cs
@@ -11,4 +11,36 @@ public class DataContext : DbContext
     }
 
     public DbSet<Todo>? Todos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Todo>()
+            .Property(x => x.Id)
+        ;
+        modelBuilder
+            .Entity<Todo>()
+            .Property(x => x.User)
+            .HasMaxLength(120)
+            .HasColumnType("varchar(120)")
+        ;
+        modelBuilder
+            .Entity<Todo>()
+            .Property(x => x.Title)
+            .HasColumnType("varchar(160)")
+        ;
+        modelBuilder
+            .Entity<Todo>()
+            .Property(x => x.Done)
+            .HasColumnType("bit")
+        ;
+        modelBuilder
+            .Entity<Todo>()
+            .Property(x => x.Date)
+        ;
+        modelBuilder
+            .Entity<Todo>()
+            .HasIndex(x => x.User)
+        ;
+    }
 }
