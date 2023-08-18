f4c0e1d66e9955cbdb6d929ae9e4bfd1f867de7b
elvesbd <elvesbd@yahoo.com>
2023-08-18 15:27:16 -0300

    feat: given an invalid command should be return false

--- TodoApp.Domain.Tests/CommandTests/UpdateTodoCommandTest.cs
+++ TodoApp.Domain.Tests/CommandTests/UpdateTodoCommandTest.cs
@@ -1,24 +1,17 @@
 using TodoApp.Domain.Commands;
 
-namespace TodoApp.Domain.Tests.CommandsTests;
+namespace TodoApp.Domain.Tests.CommandTests;
 
 [TestClass]
 public class UpdateTodoCommandTests
 {
-    private readonly CreateTodoCommand _invalidCommand = new("", "", DateTime.Now);
-    private readonly CreateTodoCommand _validCommand = new("Create Repo", "Elves Brito", DateTime.Now);
+    private readonly UpdateTodoCommand _invalidCommand = new(Guid.NewGuid(), "", "");
+    private readonly UpdateTodoCommand _validCommand = new(Guid.NewGuid(), "Todo Title", "Elves Brito");
 
     [TestMethod]
     public void Given_an_invalid_command()
     {
         _invalidCommand.Validate();
-        Assert.AreEqual(_invalidCommand.IsValid, false);
-    }
-
-    [TestMethod]
-    public void Given_an_valid_command()
-    {
-        _validCommand.Validate();
-        Assert.AreEqual(_validCommand.IsValid, true);
+        Assert.IsFalse(_invalidCommand.IsValid);
     }
 }
