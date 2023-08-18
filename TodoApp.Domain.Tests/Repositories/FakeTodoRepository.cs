97fbad9860303b684c0ef41c05ee265b5284d5fc
elvesbd <elvesbd@yahoo.com>
2023-08-18 11:19:45 -0300

    feat: created Fake todo repository

---
+++ TodoApp.Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -0,0 +1,17 @@
+using TodoApp.Domain.Entities;
+using TodoApp.Domain.Repositories;
+
+namespace TodoApp.Domain.Tests.Repositories;
+
+public class FakeTodoRepository : ITodoRepository
+{
+    public void Create(Todo todo)
+    {
+
+    }
+
+    public void Update(Todo todo)
+    {
+
+    }
+}
