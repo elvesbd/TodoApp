4b3f7d98b4ee6a9822a72013ebee2c69be65ecf2
elvesbd <elvesbd@yahoo.com>
2023-08-18 11:32:18 -0300

    refactor: simplifying the tests

--- TodoApp.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ TodoApp.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -7,21 +7,21 @@ namespace TodoApp.Domain.Tests.HandlerTests;
 [TestClass]
 public class CreateTodoHandlerTests
 {
+    private readonly CreateTodoCommand _invalidCommand = new("", "", DateTime.Now);
+    private readonly CreateTodoCommand _validCommand = new("Create Repo", "Elves Brito", DateTime.Now);
+    private readonly TodoHandler _handler = new(new FakeTodoRepository());
+
     [TestMethod]
     public void Given_an_invalid_command_must_stop_execution()
     {
-        var command = new CreateTodoCommand("", "", DateTime.Now);
-        var handler = new TodoHandler(new FakeTodoRepository());
-        var result = (GenericCommandResult)handler.Handle(command);
+        var result = (GenericCommandResult)_handler.Handle(_invalidCommand);
         Assert.AreEqual(result.Success, false);
     }
 
     [TestMethod]
     public void Given_an_valid_command_must_create_an_todo()
     {
-        var command = new CreateTodoCommand("Create Repo", "Elves Brito", DateTime.Now);
-        var handler = new TodoHandler(new FakeTodoRepository());
-        var result = (GenericCommandResult)handler.Handle(command);
+        var result = (GenericCommandResult)_handler.Handle(_validCommand);
         Assert.AreEqual(result.Success, true);
     }
 }
