7b2033c7934d44e1ee0cce0f1becac4d80ea9566
elvesbd <elvesbd@yahoo.com>
2023-08-18 15:28:20 -0300

    feat: given an valid command should be return true

--- TodoApp.Domain.Tests/CommandTests/UpdateTodoCommandTest.cs
+++ TodoApp.Domain.Tests/CommandTests/UpdateTodoCommandTest.cs
@@ -9,9 +9,16 @@ public class UpdateTodoCommandTests
     private readonly UpdateTodoCommand _validCommand = new(Guid.NewGuid(), "Todo Title", "Elves Brito");
 
     [TestMethod]
-    public void Given_an_invalid_command()
+    public void Given_an_invalid_command_should_be_return_false()
     {
         _invalidCommand.Validate();
         Assert.IsFalse(_invalidCommand.IsValid);
+    }
+
+    [TestMethod]
+    public void Given_an_valid_command_should_be_return_true()
+    {
+        _validCommand.Validate();
+        Assert.IsTrue(_validCommand.IsValid);
     }
 }
