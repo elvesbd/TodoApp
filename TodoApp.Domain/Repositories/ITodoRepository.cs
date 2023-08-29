30e6e045a245d008be4a249035e549e24d80164a
elvesbd <elvesbd@yahoo.com>
2023-08-29 18:35:50 -0300

    fet: implemented new methods

--- TodoApp.Domain/Repositories/ITodoRepository.cs
+++ TodoApp.Domain/Repositories/ITodoRepository.cs
@@ -7,4 +7,8 @@ public interface ITodoRepository
     void Create(Todo todo);
     void Update(Todo todo);
     Todo GetById(Guid id, string user);
+    IEnumerable<Todo> GetAll(string user);
+    IEnumerable<Todo> GetAllDone(string user);
+    IEnumerable<Todo> GetAllUndone(string user);
+    IEnumerable<Todo> GetByPeriod(string user, DateTime date, bool done);
 }
