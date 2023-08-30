ad2f20dc6c188362496f44cc611934349b6973dc
elvesbd <elvesbd@yahoo.com>
2023-08-30 10:20:08 -0300

    feat: finalized controller methods

--- TodoApp.Domain.Api/Controllers/TodoController.cs
+++ TodoApp.Domain.Api/Controllers/TodoController.cs
@@ -4,29 +4,106 @@ using TodoApp.Domain.Entities;
 using TodoApp.Domain.Handlers;
 using TodoApp.Domain.Repositories;
 
-namespace TodoApp.Domain.Controllers;
-
-[ApiController]
-public class TodoController : ControllerBase
+namespace TodoApp.Domain.Controllers
 {
-    private readonly TodoHandler _handler;
-    private readonly ITodoRepository _repository;
-    public TodoController(TodoHandler handler, ITodoRepository repository)
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
     {
-        _handler = handler;
-        _repository = repository;
-    }
+        private readonly TodoHandler _handler;
+        private readonly ITodoRepository _repository;
 
-    [HttpGet("v1/todos")]
-    public IEnumerable<Todo> GetAll()
-    {
-        return _repository.GetAll("elvesbrito");
-    }
+        public TodoController(TodoHandler handler, ITodoRepository repository)
+        {
+            _handler = handler;
+            _repository = repository;
+        }
 
-    [HttpPost("v1/todos")]
-    public GenericCommandResult Create([FromBody] CreateTodoCommand command)
-    {
-        command.User = "elvesbrito";
-        return (GenericCommandResult)_handler.Handle(command);
+        [HttpGet]
+        public IEnumerable<Todo> GetAll()
+        {
+            return _repository.GetAll("elvesbrito");
+        }
+
+        [HttpGet("done")]
+        public IEnumerable<Todo> GetAllDone()
+        {
+            return _repository.GetAllDone("elvesbrito");
+        }
+
+        [HttpGet("undone")]
+        public IEnumerable<Todo> GetAllUndone()
+        {
+            return _repository.GetAllUndone("elvesbrito");
+        }
+
+        [HttpGet("done/today")]
+        public IEnumerable<Todo> GetDoneForToday()
+        {
+            return _repository.GetByPeriod
+            (
+                "elvesbrito",
+                DateTime.Now.Date,
+                true
+            );
+        }
+
+        [HttpGet("undone/today")]
+        public IEnumerable<Todo> GetUndoneForToday()
+        {
+            return _repository.GetByPeriod
+            (
+                "elvesbrito",
+                DateTime.Now.Date,
+                false
+            );
+        }
+
+        [HttpGet("done/tomorrow")]
+        public IEnumerable<Todo> GetDoneForTomorrow()
+        {
+            return _repository.GetByPeriod
+            (
+                "elvesbrito",
+                DateTime.Now.Date.AddDays(1),
+                true
+            );
+        }
+
+        [HttpGet("undone/tomorrow")]
+        public IEnumerable<Todo> GetUndoneForTomorrow()
+        {
+            return _repository.GetByPeriod
+            (
+                "elvesbrito",
+                DateTime.Now.Date.AddDays(1),
+                false
+            );
+        }
+
+        [HttpPost]
+        public GenericCommandResult Create([FromBody] CreateTodoCommand command)
+        {
+            command.User = "elvesbrito";
+            return (GenericCommandResult)_handler.Handle(command);
+        }
+
+        [HttpPut]
+        public GenericCommandResult Update([FromBody] UpdateTodoCommand command)
+        {
+            return (GenericCommandResult)_handler.Handle(command);
+        }
+
+        [HttpPut("mark-as-done")]
+        public GenericCommandResult MarkAsDone([FromBody] MarkTodoAsDoneCommand command)
+        {
+            return (GenericCommandResult)_handler.Handle(command);
+        }
+
+        [HttpPut("mark-as-undone")]
+        public GenericCommandResult MarkAsUndone([FromBody] MarkTodoAsUndoneCommand command)
+        {
+            return (GenericCommandResult)_handler.Handle(command);
+        }
     }
 }
