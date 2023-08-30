380db79a97fcc899ee7ed2685ee58b0f32b0498d
elvesbd <elvesbd@yahoo.com>
2023-08-30 11:54:38 -0300

    feat: created migrations and update database

--- TodoApp.Domain.Api/Program.cs
+++ TodoApp.Domain.Api/Program.cs
@@ -8,9 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 //builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("database"));
-builder.Services.AddDbContext<DataContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("connectionString"))
-);
+/* builder.Services.AddDbContext<DataContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+); */
+builder.Services.AddEntityFrameworkSqlServer()
+    .AddDbContext<DataContext>(
+        options => options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    );
 builder.Services.AddTransient<ITodoRepository, TodoRepository>();
 builder.Services.AddTransient<TodoHandler, TodoHandler>();
 builder.Services.AddEndpointsApiExplorer();
